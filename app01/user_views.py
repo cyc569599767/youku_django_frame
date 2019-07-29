d849921b2fa11b0152cd2b718559fa51ec967a5b
Yaco <569599767@qq.com>
2019-07-29 08:35:57 +0800

    去除错误代码

--- app01/user_views.py
+++ app01/user_views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render, redirect, HttpResponse
 from app01 import models
 
-
-def wrapper(func):
-    def inner(*args, **kwargs):
-        if not userinfo['username']:
-            return redirect('/index/')
-        res = func(*args, **kwargs)
-        return res
-
-    return inner
